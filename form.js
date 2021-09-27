a8be9fb7c1563e2fea73f59fae941f9b6f42a0f0
teymie <teymie2035@gmail.com>
2021-09-27 10:09:17 -0700

    changes made

--- form.js
+++ form.js
@@ -2,70 +2,51 @@ const myName = document.getElementById("name");
 const phone = document.getElementById("number");
 const email = document.getElementById("email");
 const submit = document.getElementById("submit");
-submit.addEventListener("click", function validate(e) {
-  // e.preventDefault();
+submit.addEventListener("click", validator);
+function validator(e) {
+  e.preventDefault();
   let text;
   let letters = /^[A-za-z]+$/;
   if (myName.value == "") {
     text = "Name cannot be empty!";
     document.getElementById("demo").innerText = text;
     myName.focus();
-    return false;
   }
-  if (myName.value.match(letters)) {
+  let checkName = myName.value.match(letters);
+  if (checkName) {
     text = "";
     document.getElementById("demo").innerText = text;
-    return false;
   } else {
     text = "Please input alphabet characters only";
     document.getElementById("demo").innerText = text;
     myName.focus();
-    return false;
   }
-});
-submit.addEventListener("click", function valiDate(e) {
-  // e.preventDefault();
-  if (phone.value == "" || isNaN(phone.value) || phone.value.length !== 11) {
+  let checkNumber =
+    phone.value == "" || isNaN(phone.value) || phone.value.length !== 11;
+  if (checkNumber) {
     text = "Please enter a valid 11 digit number!";
     document.getElementById("demo2").innerText = text;
     phone.focus();
     document.getElementById("demo2").innerText = text;
     phone.focus();
-    return false;
   } else {
     text = "";
     document.getElementById("demo2").innerText = text;
-    phone.focus();
-    return false;
   }
-});
-submit.addEventListener("click", function validAte(e) {
-  // e.preventDefault();
   let atposition = email.value.indexOf("@");
   let dotposition = email.value.lastIndexOf(".");
-  if (atposition < 1 || dotposition - atposition < 2 || email.value == "") {
+  let checkEmail =
+    atposition < 1 || dotposition - atposition < 2 || email.value == "";
+  if (checkEmail) {
     text = "Please enter a valid Email!";
     document.getElementById("demo3").innerHTML = text;
     email.focus();
-    return false;
   } else {
     text = "";
     document.getElementById("demo3").innerText = text;
     email.focus();
-    return false;
   }
-});
-
-// submit.addEventListener("click", function formz(e) {
-//   e.preventDefault();
-//   let myname = document.Regform.name.value;
-//   let text;
-//   if (myname == "tolulope") {
-//     text = "correct !";
-//     // return false;
-//   } else {
-//     text = "Enter a correct name !";
-//     // return false;
-//   }
-//   document.getElementById("demo").innerHTML = text;
-// });
+  if (checkName && checkNumber != true && checkEmail != true) {
+    alert("submitted!");
+  }
+}
